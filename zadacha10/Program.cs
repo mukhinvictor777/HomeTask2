4b28f15ad6e6e31ad893f94eeee0812f9a58eb37
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-08 13:57:19 +0600

    решил задачу 10

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getSecondDigitNumber (int userNumber)
+{
+    int secondDigit = userNumber % 100 / 10;
+    if (secondDigit < 0) secondDigit = secondDigit * -1;
+    return secondDigit;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if ((userNumber>99 && userNumber<1000)||(userNumber>-1000 && userNumber<-99))
+{
+    int secondDigit = getSecondDigitNumber (userNumber);
+    Console.WriteLine ($"{userNumber} -> {secondDigit}");
+}
+else
+{
+    Console.WriteLine ("Вы ввели не трехзначное число. Запустите программу заново и введите трехзначное число");
+}
