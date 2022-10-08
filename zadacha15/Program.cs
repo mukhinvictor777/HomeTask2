fc2a384ab7c344393e763b3137ee20ae62da6759
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-08 15:21:25 +0600

    Решил задачу 15

--- zadacha15/Program.cs
+++ zadacha15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру от 1 до 7");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if ( userNumber < 1 || userNumber > 7)
+{
+    Console.WriteLine ("В неделе 7 дней, перезапустите программу и введите цифру от 1 до 7");
+}
+else
+{
+    if (userNumber == 6 || userNumber == 7)
+    {
+         Console.WriteLine ($"{userNumber} -> да");
+    }
+    else
+    {
+        Console.WriteLine ($"{userNumber} -> нет");
+    }
+}
